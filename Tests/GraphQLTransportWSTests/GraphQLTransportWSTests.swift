c470e17fa5d694051c16cc83b5997286a61bd074
Jay Herron <NeedleInAJayStack@protonmail.com>
2025-09-22 23:56:58 -0600

    test: streaming test uses dynamic wait

--- Tests/GraphQLTransportWSTests/GraphQLTransportWSTests.swift
+++ Tests/GraphQLTransportWSTests/GraphQLTransportWSTests.swift
@@ -10,6 +10,7 @@ class GraphqlTransportWSTests: XCTestCase {
     var serverMessenger: TestMessenger!
     var server: Server<TokenInitPayload, AsyncThrowingStream<GraphQLResult, Error>>!
     var context: TestContext!
+    var subscribeReady: Bool! = false
 
     override func setUp() {
         // Point the client and server at each other
@@ -30,10 +31,12 @@ class GraphqlTransportWSTests: XCTestCase {
                 )
             },
             onSubscribe: { graphQLRequest in
-                try await api.subscribe(
+                let subscription = try await api.subscribe(
                     request: graphQLRequest.query,
                     context: context
                 ).get()
+                self.subscribeReady = true
+                return subscription
             }
         )
         self.context = context
@@ -172,8 +175,15 @@ class GraphqlTransportWSTests: XCTestCase {
                     id: id
                 )
 
-                // Short sleep to allow for server to register subscription
-                usleep(3000)
+                // Wait until server has registered subscription
+                var i = 0
+                while !self.subscribeReady, i < 50 {
+                    usleep(1000)
+                    i = i + 1
+                }
+                if i == 50 {
+                    XCTFail("Subscription timeout: Took longer than 50ms to set up")
+                }
 
                 self.context.publisher.emit(event: "hello \(dataIndex)")
             }
